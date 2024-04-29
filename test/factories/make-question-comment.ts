7afeb9ca8d7d3935b949ae23c5d0ab0dbcecac8c
vitorlinsbinski <vitor.linsbinski@hotmail.com>
2024-04-29 08:40:30 -0400

    add: delete-question-comment controller

--- test/factories/make-question-comment.ts
+++ test/factories/make-question-comment.ts
@@ -29,7 +29,7 @@ export function makeQuestionComment(
 export class QuestionCommentFactory {
   constructor(private prisma: PrismaService) {}
 
-  async makePrismaQuestion(
+  async makePrismaQuestionComment(
     data: Partial<QuestionCommentProps> = {},
   ): Promise<QuestionComment> {
     const questionComment = makeQuestionComment(data);
