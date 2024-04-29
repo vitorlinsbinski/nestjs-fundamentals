215842832b1dad9a0729d8a655d46cfd7f091121
vitorlinsbinski <vitor.linsbinski@hotmail.com>
2024-04-29 08:29:03 -0400

    add: comment-on-question controller

--- src/domain/forum/application/use-cases/comment-on-question.ts
+++ src/domain/forum/application/use-cases/comment-on-question.ts
@@ -4,6 +4,7 @@ import { QuestionCommentsRepository } from '../repositories/question-comments-re
 import { UniqueEntityId } from '@/core/entities/unique-entity-id';
 import { ResourceNotFoundError } from '@/core/errors/resource-not-found-error';
 import { Either, left, right } from '@/core/either';
+import { Injectable } from '@nestjs/common';
 
 interface CommentOnQuestionUseCaseRequest {
   authorId: string;
@@ -16,6 +17,7 @@ type CommentOnQuestionUseCaseResponse = Either<
   { questionComment: QuestionComment }
 >;
 
+@Injectable()
 export class CommentOnQuestionUseCase {
   constructor(
     private questionsRepository: QuestionsRepository,
--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -25,6 +25,8 @@ import { EditAnswerUseCase } from '@/domain/forum/application/use-cases/edit-ans
 import { EditAnswerController } from './controllers/edit-answer.controller';
 import { ChooseQuestionBestAnswerController } from './controllers/choose-question-best-answer.controller';
 import { ChooseQuestionBestAnswerUseCase } from '@/domain/forum/application/use-cases/choose-question-best-answer';
+import { CommentOnQuestionController } from './controllers/comment-on-question.controller';
+import { CommentOnQuestionUseCase } from '@/domain/forum/application/use-cases/comment-on-question';
 
 @Module({
   imports: [DatabaseModule, CryptographyModule],
@@ -40,7 +42,8 @@ import { ChooseQuestionBestAnswerUseCase } from '@/domain/forum/application/use-
     EditAnswerController,
     DeleteAnswerController,
     FetchRecentQuestionAnswersController,
-    ChooseQuestionBestAnswerController
+    ChooseQuestionBestAnswerController,
+    CommentOnQuestionController,
   ],
   providers: [
     CreateQuestionUseCase,
@@ -54,7 +57,8 @@ import { ChooseQuestionBestAnswerUseCase } from '@/domain/forum/application/use-
     EditAnswerUseCase,
     DeleteAnswerUseCase,
     FetchQuestionAnswersUseCase,
-    ChooseQuestionBestAnswerUseCase
+    ChooseQuestionBestAnswerUseCase,
+    CommentOnQuestionUseCase,
   ],
 })
 export class HttpModule {}
