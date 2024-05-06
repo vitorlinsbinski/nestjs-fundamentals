675e00d90020c22d3f40fdd6a797c718820a514a
vitorlinsbinski <vitor.linsbinski@hotmail.com>
2024-05-06 09:01:49 -0400

    add: persistence of question-attachments at the  domain layer

--- src/domain/forum/application/repositories/question-attachments-repository.ts
+++ src/domain/forum/application/repositories/question-attachments-repository.ts
@@ -1,8 +1,13 @@
 import { QuestionAttachment } from '../../enterprise/entities/question-attachment';
 
 export abstract class QuestionAttachmentsRepository {
+  abstract createMany(attachments: QuestionAttachment[]): Promise<void>;
+
+  abstract deleteMany(attachments: QuestionAttachment[]): Promise<void>;
+
   abstract findManyByQuestionId(
     questionId: string,
   ): Promise<QuestionAttachment[]>;
+
   abstract deleteManyByQuestionId(questionId: string): Promise<void>;
 }
--- src/domain/forum/application/use-cases/edit-question.spec.ts
+++ src/domain/forum/application/use-cases/edit-question.spec.ts
@@ -85,4 +85,45 @@ describe('Edit Question', () => {
     expect(result.isLeft()).toBe(true);
     expect(result.value).toBeInstanceOf(NotAllowedError);
   });
+
+  it('should sync new and removed attachments when editing a question', async () => {
+    const newQuestion = makeQuestion(
+      { authorId: new UniqueEntityId('author-1') },
+      new UniqueEntityId('question-1'),
+    );
+
+    await inMemoryQuestionsRepository.create(newQuestion);
+
+    inMemoryQuestionAttachmentsRepository.items.push(
+      makeQuestionAttachment({
+        questionId: newQuestion.id,
+        attachmentId: new UniqueEntityId('1'),
+      }),
+      makeQuestionAttachment({
+        questionId: newQuestion.id,
+        attachmentId: new UniqueEntityId('2'),
+      }),
+    );
+
+    const result = await sut.execute({
+      authorId: 'author-1',
+      title: 'Pergunta teste',
+      content: 'Conteúdo teste',
+      questionId: newQuestion.id.toValue(),
+      attachmentsIds: ['1', '3'],
+    });
+
+    expect(result.isRight()).toBe(true);
+    expect(inMemoryQuestionAttachmentsRepository.items).toHaveLength(2);
+    expect(inMemoryQuestionAttachmentsRepository.items).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          attachmentId: new UniqueEntityId('1'),
+        }),
+        expect.objectContaining({
+          attachmentId: new UniqueEntityId('3'),
+        }),
+      ]),
+    );
+  });
 });
